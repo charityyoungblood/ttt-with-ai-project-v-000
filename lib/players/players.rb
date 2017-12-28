39a2c4c263bf4f4e92e6244f130d6bbedadb9c66
charityyoungblood <youngblood.charity@gmail.com>
2017-12-28 13:03:11 -0800

    added iterator to display value of open cells

--- lib/players/players.rb
+++ lib/players/players.rb
@@ -10,7 +10,25 @@ module Players
   end
   
   class Computer < Player
+    def initialize(token) # initialize with token
+      @token = token 
+    end
     
+    def move(board) # this method will take a board object as an argument
+      binding.pry
+      # get program to return all open cells 
+      board.cells.find_all do |r, index|
+        if r.include?(" ")
+          r[index]
+        end 
+#      unless board.cells.include?("X") || board.cells.include?("O")
+#      if valid_move?(index) # check to see if position is occupied 
+#      # return string of open position
+#        index
+      end
+    end
+        # go through the cells array and see if position is taken, if not - return open cells
+    end
   end 
   
 end 
