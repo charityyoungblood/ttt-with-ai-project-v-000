2f115378dba45a545f3c57ec8a44d1cde6c7d342
charityyoungblood <youngblood.charity@gmail.com>
2017-12-28 12:02:34 -0800

    added logic to move method in players.rb

--- lib/players/players.rb
+++ lib/players/players.rb
@@ -1,1 +1,15 @@
 # this should be a module 
+
+module Players 
+  
+  class Human < Player
+    def move(index) 
+      binding.pry
+      user_input = gets.chomp.to_i
+      turn = user_input - 1
+      @cells[turn]
+    end 
+  end
+  
+  
+end 
