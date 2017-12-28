23733ed8bb2f924f3e598c4aba1ea7e3fce8e7e7
charityyoungblood <youngblood.charity@gmail.com>
2017-12-27 16:20:39 -0800

    added @cells to initialize method

--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,15 @@
 class Board 
 
   def initialize 
-    
+    @cells = [" "," "," "," "," "," "," "," "," "]
   end 
   
-  def cells # this method should set the cells attribute to an empty array
-    @cells = []
+  def cells # this method should set the cells attribute to an empty array - getter method - gets the value from data
+    @cells
   end 
   
   def cells=(cells)
-    @cells = cells
+    @cells = [] # setter method - sets the data to a value 
   end 
   
   def reset! # this method should clear the board - sets all cells to " "
